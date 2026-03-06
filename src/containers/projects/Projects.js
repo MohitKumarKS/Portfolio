894889c35e6657decdceebb193696a36467e89e6
Mohit Kumar KS <mohitkumarks2024cse@sece.ac.in>
2026-03-06 20:18:36 +0530

    first change after deploy error

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -1,10 +1,3 @@
-import React from "react";
-import "./Project.scss";
-import {openSource} from "../../portfolio";
-
 export default function Projects() {
-  if (!openSource.display) {
-    return null;
-  }
   return null;
 }
